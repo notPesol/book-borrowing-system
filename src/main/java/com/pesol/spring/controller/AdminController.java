a98b080f4425b39c36bf90a5ad4f49160d07c9e5
notPesol <pesol2012@hotmail.com>
2022-04-14 12:34:45 +0700

    finish CRUD category

--- src/main/java/com/pesol/spring/controller/AdminController.java
+++ src/main/java/com/pesol/spring/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,34 +22,93 @@ public class AdminController {
     @Autowired
     private BookCategoryService bookCategoryService;
     
+    // For render admin home page
     @GetMapping
     public String renderHome() {
         return "admin/home";
     }
 
-    @GetMapping("/add-category")
-    public String renderAddCategory(Model model) {
-        model.addAttribute("tempCategory", new BookCategory());
-        return "admin/add-category";
+    // For render categories page
+    @GetMapping("/category")
+    public String renderCategory(Model model) {
+        model.addAttribute("categories", bookCategoryService.getAll());
+        return "admin/category/home";
     }
 
-    @PostMapping("/add-category")
-    public String processAddCategory(@Valid @ModelAttribute BookCategory tempCategory,
+    // For render add category page
+    @GetMapping("/category/add-category")
+    public String renderAddCategory(Model model) {
+        model.addAttribute("tempCategory", new BookCategory());
+        return "admin/category/add-category";
+    }
+
+    // For process add the category page
+    @PostMapping("/category/add-category")
+    public String processAddCategory(@Valid @ModelAttribute(name = "tempCategory") BookCategory tempCategory,
              BindingResult bindingResult, Model model) {
         
         if (bindingResult.hasErrors()) {
-            return "admin/add-category";
+            System.out.println(bindingResult);
+            return "admin/category/add-category";
         }
 
         BookCategory category = bookCategoryService.getByName(tempCategory.getName());
         if (category != null) {
             model.addAttribute("tempCategory", new BookCategory());
-            model.addAttribute("errorMessage", "Category name is already exist");
-            return "admin/add-category";
+            model.addAttribute("errorMessage", "Category: " + tempCategory.getName() + " is already exist");
+            return "admin/category/add-category";
         }
 
-        //bookCategoryService.save(tempCategory);
+        bookCategoryService.save(tempCategory);
 
-        return "redirect:/admin/add-category?success";
+        return "redirect:/admin/category/add-category?success";
+    }
+
+    // For render edit category page
+    @GetMapping("/category/{id}")
+    public String renderEditCategory(@PathVariable int id, Model model) {
+
+        BookCategory tempCategory = bookCategoryService.getById(id);
+
+        if (tempCategory == null) {
+            return "redirect:/admin/category";
+        }
+
+        model.addAttribute("tempCategory", tempCategory);
+
+        return "admin/category/edit-category";
+    }
+
+    // For process update the category
+    @PostMapping("/category/update")
+    public String processUpdateCategory(@Valid @ModelAttribute(name = "tempCategory") BookCategory tempCategory,
+            BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "admin/category/edit-category";
+        }
+
+        BookCategory category = bookCategoryService.getByName(tempCategory.getName());
+        if(category != null) {
+            model.addAttribute("errorMessage", "Category " + tempCategory.getName() + " is already exist");
+            return "admin/category/edit-category";
+        }
+
+        bookCategoryService.save(tempCategory);
+
+        return "redirect:/admin/category?updated";
+    }
+
+    // For process delete the category
+    @PostMapping("/category/{id}")
+    public String processDeleteCategory(@PathVariable int id) {
+        
+        BookCategory category = bookCategoryService.getById(id);
+
+        if(category != null) {
+            bookCategoryService.delete(category);
+        }
+
+        return "redirect:/admin/category?deleted";
     }
 }
--- src/main/java/com/pesol/spring/entity/BookCategory.java
+++ src/main/java/com/pesol/spring/entity/BookCategory.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "book_category")
@@ -14,6 +16,8 @@ public class BookCategory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank(message = "is required")
+    @Size(min = 2, message = "length must greater than 1")
     private String name;
 
     public BookCategory() {
